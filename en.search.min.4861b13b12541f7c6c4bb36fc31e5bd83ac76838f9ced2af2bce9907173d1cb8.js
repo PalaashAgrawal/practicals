362d9720ecdc644d204be0c668ce57a205ceb65a
Palaash Agrawal <palaashagrawal29@gmail.com>
2021-04-20 11:51:09 +0000

    Week 13 v2

--- en.search.min.4861b13b12541f7c6c4bb36fc31e5bd83ac76838f9ced2af2bce9907173d1cb8.js
+++ en.search.min.4861b13b12541f7c6c4bb36fc31e5bd83ac76838f9ced2af2bce9907173d1cb8.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.50ef364549ed34d9e9148fcd5e5662da373b6022ceb09d5ba212216827d122ea.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.9245f7dd21125c84c5e00f7bfd552b0e4faf076bdb85fa18387b8d6ab8f576ff.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
